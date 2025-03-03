12706fb964db31044492ec87148930bdc440d5e3
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-03 11:43:02 +0530

    UC3

--- src/main/java/com/example/HelloWorldProject/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldProject/HelloWorldController.java
@@ -1,15 +1,17 @@
 package com.example.HelloWorldProject;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String sayHello(Model model) {
-        model.addAttribute("message", "Hello from BridgeLabz");
-        return "hello";
+    public String sayHello() {
+        return "Hello from BridgeLabz";
     }
 }
+
